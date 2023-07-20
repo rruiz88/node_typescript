ab222a69807ab45cc67b902d7308471b127e0f7b
rruiz88 <roman.ruiz816@gmail.com>
2023-07-20 13:23:07 -0500

    installing express.

--- app.js
+++ app.js
@@ -1,14 +1,8 @@
 "use strict";
-const num1E = document.getElementById("num1");
-const num2E = document.getElementById("num2");
-//! ignore null element
-const btnEl = document.querySelector("button");
-function add(num1, num2) {
-    return num1 + num2;
-}
-btnEl.addEventListener("click", () => {
-    const num1 = num1E.value;
-    const num2 = num2E.value;
-    const result = add(+num1, +num2);
-    console.log(result);
-});
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+app.listen({ port: 3000 });
--- app.ts
+++ app.ts
@@ -1,15 +1,5 @@
-const num1E = document.getElementById("num1") as HTMLInputElement;
-const num2E = document.getElementById("num2") as HTMLInputElement;
-//! ignore null element
-const btnEl = document.querySelector("button")!;
+import express from "express";
 
-function add(num1: number, num2: number) {
-  return num1 + num2;
-}
+const app = express();
 
-btnEl.addEventListener("click", () => {
-  const num1 = num1E.value;
-  const num2 = num2E.value;
-  const result = add(+num1, +num2);
-  console.log(result);
-});
+app.listen({ port: 3000 });
