83d10965311c470e83ab65bb4ff98ee416bf06d9
rruiz88 <roman.ruiz816@gmail.com>
2023-07-21 12:51:28 -0500

    model and routes added.

--- app.ts
+++ app.ts
@@ -1,5 +1,10 @@
 import express from "express";
+import todoRoutes from "./routes/todos";
+import bodyParser from "body-parser";
 
 const app = express();
+app.use(bodyParser.json());
+
+app.use(todoRoutes);
 
 app.listen({ port: 3000 });
